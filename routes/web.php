acdd413f2cfa9ba0f090b6079dcc217d545ee269
Nama Anda <fahriabdurahmansoleh@gmail.com>
2025-10-12 21:13:26 +0700

    adding read update delete

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::get('/store/invoice', [OrderController::class, 'print'])->name('order.pri
 // items
 Route::get('/items',[ItemController::class, 'index'])->name('items.index');
 Route::post('/items/store',[ItemController::class, 'store'])->name('items.store');
+Route::put('/items/{id}', [ItemController::class, 'update'])->name('items.update');
+Route::delete('/items/{id}/delete',[ItemController::class, 'destroy'])->name('items.destroy');
+Route::get('/qrcode/{uuid}', [ItemController::class, 'showQr'])->name('items.qrcode');
